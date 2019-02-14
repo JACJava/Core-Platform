a36cd513b8a5d2bb1d0c9e76af09dda3cb0200ee
JACJava <46228469+JACJava@users.noreply.github.com>
2019-02-14 17:06:44 -0600

    Add files via upload

--- src/com/pluralsight/CorePlatform/Main.java
+++ src/com/pluralsight/CorePlatform/Main.java
@@ -1,8 +1,41 @@
 package com.pluralsight.CorePlatform;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+
+	char[] buff = new char[8];
+	int length;
+	Reader reader = null;
+
+	try {
+        reader = new BufferedReader(new FileReader("file1.txt"));
+        while ((length = reader.read(buff)) >= 0) {
+            System.out.println("\nlength:  " + length);
+            for (int i = 0; i < length; i++)
+                System.out.print(buff[i]);
+        }
+    } catch(IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " -- " + e.getMessage());
+    } finally {
+	    try {
+	        if (reader != null)
+	            reader.close();
+        } catch(IOException e2) {
+            System.out.println(e2.getClass().getSimpleName() + " -- " + e2.getMessage());
+        }
+    }
+
+
+
+
+
+
     }
 }
